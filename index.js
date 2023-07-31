2aabbb456a7c93f06534ba868619d038fa06390d
Tano Paul <tanopaul38@gmail.com>
2023-07-31 18:11:46 -0400

    completed continued functions phase 1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function innerFunc(adj = 'special') {
+        return `You are ${flair}${adj}${flair}!`;
+    }
+}
